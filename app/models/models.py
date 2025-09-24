29cb3a6023f8cd70b88cff961822807908445ebd
EsneiderCdD <ecd.97@hotmail.com>
2025-09-24 10:19:10 -0500

    feat: tablas relacionales, nuevas tablas

--- app/models/models.py
+++ app/models/models.py
@@ -25,3 +25,43 @@ class Nota(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     oferta_id = db.Column(db.Integer, db.ForeignKey('ofertas.id'))
     nota = db.Column(db.Text)
+
+
+# 🔹 NUEVAS TABLAS 🔹
+
+class AnalisisOferta(db.Model):
+    __tablename__ = 'analisis_ofertas'
+    id = db.Column(db.Integer, primary_key=True)
+    oferta_id = db.Column(db.Integer, db.ForeignKey('ofertas.id'), unique=True, nullable=False)
+    fecha_analisis = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc))
+
+    ciudad = db.Column(db.String(255))
+    modalidad = db.Column(db.String(100))
+    cargo = db.Column(db.String(255))
+
+    oferta = db.relationship("Oferta", backref=db.backref("analisis", uselist=False))
+
+
+class CategoriaTecnologia(db.Model):
+    __tablename__ = 'categorias_tecnologia'
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(100), unique=True, nullable=False)
+
+
+class Tecnologia(db.Model):
+    __tablename__ = 'tecnologias'
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(100), unique=True, nullable=False)
+    categoria_id = db.Column(db.Integer, db.ForeignKey('categorias_tecnologia.id'), nullable=False)
+
+    categoria = db.relationship("CategoriaTecnologia", backref="tecnologias")
+
+
+class OfertaTecnologia(db.Model):
+    __tablename__ = 'ofertas_tecnologias'
+    id = db.Column(db.Integer, primary_key=True)
+    oferta_id = db.Column(db.Integer, db.ForeignKey('ofertas.id'), nullable=False)
+    tecnologia_id = db.Column(db.Integer, db.ForeignKey('tecnologias.id'), nullable=False)
+
+    oferta = db.relationship("Oferta", backref="ofertas_tecnologias")
+    tecnologia = db.relationship("Tecnologia", backref="tecnologias_ofertas")
